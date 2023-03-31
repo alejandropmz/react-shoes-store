bd0ea9bb150a5ccbad8d477fe86abd9b8f87fbe5
alejandropmz <alejandropacheco2497@gmail.com>
2023-03-30 23:22:00 -0500

    pijamas, calzados circulos para selección de menú

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -29,6 +29,23 @@ export const Inicio = () => {
         </div>
       </div>
       <div className="body">
+        {/* tenis pijamas */}
+        <div className="tenis-pijamas d-flex justify-content-center">
+          <div className="calzados d-flex flex-column justify-content-center align-items-center">
+            <img
+              src="https://www.gef.co/dx/api/dam/custom/2021/gef/es-co/imagenes/mujeres/zapatos/cuno_tenis/566x715/calzado-mujer-cuno-tenis-blanco-914-frente-gef.jpg"
+              alt=""
+            />
+            <h5>Calzados</h5>
+          </div>
+          <div className="pijamas d-flex flex-column justify-content-center align-items-center">
+            <img
+              src="https://i0.wp.com/pijamas-clover.com/wp-content/uploads/2022/07/hoodie-pijama-rey-leon.jpg?resize=600%2C600&ssl=1"
+              alt=""
+            />
+            <h5>Pijamas</h5>
+          </div>
+        </div>
         <TopBody />
         <div id="body" className="row">
           {store.productos.map((item, index) => (
